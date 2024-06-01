d63865945bb4eddf4b360f81214a81cffcc86b3b
Sean <seapat@users.noreply.github.com>
2024-06-01 20:39:20 +0200

    add delta

--- home/dot_config/wezterm/color_scheme_switch.lua
+++ home/dot_config/wezterm/color_scheme_switch.lua
@@ -1,24 +1,20 @@
 local wezterm = require 'wezterm'
-
+local module ={}
 -- wezterm.gui is not available to the mux server, so take care to
 -- do something reasonable when this config is evaluated by the mux
-function get_appearance()
-  if wezterm.gui then
-    return wezterm.gui.get_appearance()
+
+function module.dynamic_theme(config)
+  local appearance = wezterm.gui and wezterm.gui.get_appearance() or "Light"
+  if appearance:find("Dark") then
+    config.set_environment_variables.DELTA_FEATURES = "dark-mode"
+    config.color_scheme = 'Catppuccin Macchiato'
+  else 
+    config.set_environment_variables.DELTA_FEATURES = "light-mode"
+    config.color_scheme =  'Catppuccin Latte'
   end
-  return 'Dark'
+  wezterm.log_warn("Changed Environment Vars for Theming. Only works in new Program / new Tab.")
+
+  return config
 end
 
-function scheme_for_appearance(appearance)
-  if appearance:find 'Dark' then
-    return 'Bamboo'
-  else
-    return 'Bamboo Light'
-  end
-end
-
-return scheme_for_appearance(get_appearance())
--- {
---   color_scheme = scheme_for_appearance(get_appearance()),
--- }
-  
+return module
